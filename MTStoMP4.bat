70e1f10e8e9e20b51c35b7daf879c95aa54a95a8
yumetodo <yume-wikijp@live.jp>
2013-04-05 17:15:00 +0900

    とりあえず作成

---
+++ MTStoMP4.bat
@@ -0,0 +1,167 @@
+@echo off
+rem Copyright (C) 2013 yumetodo
+rem Distributed under the Boost Software License, Version 1.0.
+echo ********************************
+echo *  mts to mp4 batch Vor.0.1.0  *
+echo ********************************
+
+echo %DATE% %TIME% MTSファイルをMP4にしてみる
+
+title MTSファイルをMP4にしてみる
+
+SETLOCAL
+
+SET INPath=%~dp1
+SET OUTFILE=%~dpn1
+IF %INPath%.==. GOTO ferror
+IF "%~x1"=="mts" GOTO SETTINGa
+IF "%~x1"=="MTS" GOTO SETTINGa
+GOTO ferror
+
+:SETTINGa
+echo -----------------------------------------
+echo tsMuxeR.exeのある場所を入力して下さい
+echo -----------------------------------------
+echo 例）C:\Program Files (x86)\XviD4PSP 5\apps\tsMuxeR
+set /p PLACE1=
+IF NOT EXIST "%PLACE1%tsMuxeR.exe" goto errora
+goto SETTINGb
+
+:SETTINGb
+echo -----------------------------------------
+echo BeSweet.exeのある場所を入力して下さい
+echo -----------------------------------------
+echo 例）D:\プログラム 頻度低\BeLight
+set /p PLACE2=
+IF NOT EXIST "%PLACE2%BeSweet.exe" goto errorb
+goto SETTINGc
+
+:SETTINGc
+echo -----------------------------------------
+echo neroAacEnc.exeのある場所を入力して下さい
+echo -----------------------------------------
+echo 例）C:\Program Files (x86)\XviD4PSP 5\apps\neroAacEnc
+set /p PLACE3=
+IF NOT EXIST "%PLACE3%neroAacEnc.exe" goto errorc
+goto SETTINGd
+
+:SETTINGd
+echo ---------------------------------------------------------
+echo neroAacEnc.exeのエンコードオプションを指定してください
+echo ---------------------------------------------------------
+echo 例）-q 0.50 -lc
+echo よくわからないときは help と打ってください。空白不可です。
+set /p OPTION=
+IF %OPTION%.==. GOTO errord
+IF %OPTION%==help start http://note.chiebukuro.yahoo.co.jp/detail/n136981
+goto SETTINGe
+
+:SETTINGe
+echo -----------------------------------------
+echo MP4Box.exeのある場所を入力して下さい
+echo -----------------------------------------
+echo 例）C:\Program Files (x86)\XviD4PSP 5\apps\MP4Box
+set /p PLACE4=
+IF %PLACE4%.==. GOTO errorf
+IF NOT EXIST "%PLACE3%neroAacEnc.exe" goto errorf
+goto demux
+
+:demux
+echo "MUXOPT --no-pcr-on-video-pid --new-audio-pes --vbr --vbv-len=500" >%OUTFILE%.meta
+echo "V_MPEG4/ISO/AVC, "%OUTFILE%%~x1", fps=59.9401, insertSEI, contSPS, track=4113" >>%OUTFILE%.meta
+echo "A_AC3, "%OUTFILE%%~x1", track=4352" >>"%OUTFILE%".meta
+
+cd /d %PLACE1%
+tsMuxeR.exe "%OUTFILE%.meta" "%OUTFILE%.264"
+tsMuxeR.exe "%OUTFILE%.meta" "%OUTFILE%.ac3"
+goto convert
+
+:convert
+IF NOT EXIST "%OUTFILE%.ac3" goto erroroftsMuxeR
+cd /d %PLACE2%
+BeSweet.exe -core( -input "%OUTFILE%.ac3" -output "%OUTFILE%.wav"  -2ch -logfile "%OUTFILE%BeSweet.log" ) -azid( -s stereo -c normal -L -3db ) -ota( -hybridgain ) -ssrc( --rate 44100 )
+
+IF NOT EXIST "%OUTFILE%.wav" goto errorofBeSweet
+cd /d %PLACE3%
+neroAacEnc.exe %OPTION% -if "%OUTFILE%.wav" -of "%OUTFILE%.m4a"
+goto mux
+
+:mux
+IF NOT EXIST "%OUTFILE%.m4a" goto errorofneroAacEnc
+cd /d %PLACE3%
+MP4Box.exe -fps 24.000 -add "%OUTFILE%.264" -add "%OUTFILE%.m4a" -new "%OUTFILE%.mp4" -tmp "%INPath%1"
+IF NOT EXIST "%OUTFILE%.mp4" goto errorofMP4Box
+goto end
+
+:ferror
+echo ファイル名、拡張子等が異常です。処理をスキップします
+GOTO enderror
+
+:errora
+echo tsMuxeR.exeのある場所を正しく指定してください。
+GOTO SETTINGa
+
+:errorb
+echo BeSweet.exeのある場所を正しく指定してください。
+GOTO SETTINGb
+
+:errorc
+echo neroAacEnc.exeのある場所を正しく指定してください。
+GOTO SETTINGc
+
+:errord
+echo エンコードオプションを正しく指定してください
+GOTO SETTINGd
+
+:errorf
+echo MP4Box.exeのある場所を正しく指定してください。
+GOTO SETTINGe
+
+:erroroftsMuxeR
+echo 変換過程（tsMuxeR）でエラーが生じました。申し訳ありません。
+GOTO enderror
+
+:errorofBeSweet
+echo 変換過程（BeSweet）でエラーが生じました。申し訳ありません。
+GOTO enderror
+
+:errorofneroAacEnc
+echo 変換過程（neroAacEnc）でエラーが生じました。申し訳ありません。
+GOTO enderror
+
+:errorofMP4Box
+echo 変換過程（MP4Box）でエラーが生じました。申し訳ありません。
+GOTO enderror
+
+:end
+echo %DATE% %TIME% MTSファイルはMP4に無事変換されました。
+echo 変換元ファイルと同じ場所に出力されています。
+goto DELASK
+
+:DELASK
+echo 変換過程で生じたファイルを削除しますか？（y/n）
+set /p YORN=
+IF %YORN%==y GOTO DEL
+IF %YORN%.==n GOTO quit
+GOTO DELERROR
+
+:DELERROR
+echo 恐れ入りますが、yかnでお答えください。
+goto DELASK
+
+:enderror
+echo %DATE% %TIME% MTSファイルはMP4に変換されませんでした。
+GOTO quit
+
+:DEL
+del %OUTFILE%.meta
+del %OUTFILE%.264
+del %OUTFILE%.ac3
+del %OUTFILE%.m4a
+del %INPath%1
+echo 削除しました。
+GOTO quit
+
+:quit
+ENDLOCAL
+PAUSE
---
+++ README.md
@@ -0,0 +1,70 @@
+﻿# MTStoMP4_batch
+
+## 背景
+
+.mtsを.mp4に変換する  [http://note.chiebukuro.yahoo.co.jp/detail/n162202](http://note.chiebukuro.yahoo.co.jp/detail/n162202)  
+でやってることをバッチ化しました。
+
+## 対象とするMTSファイル
+
+映像がAVC/H.264 High  
+音声がDolby AC-3  2.0ch(2/0 L+R)  
+なら成功します。
+
+ただバッチの前半で拡張子が.mtsではない時は変換しないように設定してるので、バッチの拡張子判定の部分を消せば、tsMuxeRの対応形式なら問題なくいくと思います。
+
+## 依存するプログラム
+
+- **tsMuxeR**　[[ダウンロード↓]](http://www.videohelp.com/tools/tsMuxeR)  
+映像と音声を分離するのに使った。使い方は  
+http://freesoft.tvbok.com/movie_encode/h264/mp4-ts-m2ts-demux.html
+- **neroAacEnc.exe**　[[ダウンロード↓]](http://ftp6.nero.com/tools/NeroAACCodec-1.5.1.zip)  
+音声を.wavから.aac(.m4a)にするのに使用  
+http://note.chiebukuro.yahoo.co.jp/detail/n136981
+- **MP4Box.exe**　[[ダウンロード↓]](http://sdmr.sblo.jp/)  
+映像と音声を合体（結合）させるのに使った。MP4Box (binary | library | DL)から。使い方詳細は  
+http://note.chiebukuro.yahoo.co.jp/detail/n155574
+- **BeSweet.exe**　[[ダウンロード↓]](https://mega.nz/#!SkEkXLRD!R6M4QABUPCUVzmYw7WoUcFUbUWEq5lW0cYGcBlXo57g)  
+MD5：`0b4d96e10c3d9d5752d01eb800e83056`  
+SHA-1：`034586025ab49aec4a67b87949eb8901acd9fafb`  
+バッチファイル中で.aacを.wavに変換するのに使用。  
+**BeLight.exe**も梱包。  
+あっちこっち探しても見つからないので、結構貴重かも。  
+勝手にアップしてるので作者様から怒られないか心配。
+
+## 使い方
+基本的に`.mts`形式の動画をバッチファイルにD&Dすればいい。
+
+## LICENSE
+
+Boost Software License, Version 1.0.
+
+## memo
+
+tsMuxeR.exeの情報少なすぎ・・・。コマンドラインから使う方法がどこにも書いてないの。全く・・・。  
+[http://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q1055090665](http://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q1055090665)  
+これを見て、やっと解決策を思いつきました。
+
+demuxにffmpegを使わないのはinputファイルによってコマンドが違うとかでよくわからないから。対応形式を増やすことにあまり意味が見出せない。やりたきゃ勝手にやってくれ。
+
+```
+ffmpeg -i [FilePath]\[FileName].mts -vcodec copy -an [FilePath]\[FileName].264
+ffmpeg -i [FilePath]\[FileName].mts -acodec copy [FilePath]\[FileName].aac
+```
+
+とかすればいいんだとは思うんだけど。めんどい。
+
+## バッチファイル作成中にお世話になったサイト・文献
+
+- 指定したＵＲＬを開くコマンド｜OKWAVE  
+[http://okwave.jp/qa/q1606422.html](http://okwave.jp/qa/q1606422.html)
+- set | コマンドプロンプトを使ってみよう  
+[http://ykr414.com/dos/dos04.html#25](http://ykr414.com/dos/dos04.html#25)
+- バッチファイルの制御用コマンド  
+[http://www.fpcu.jp/dosvcmd/batch.htm#if](http://www.fpcu.jp/dosvcmd/batch.htm#if)
+- TSファイルに一括変換  
+[http://s86ed1.exblog.jp/12438664/](http://s86ed1.exblog.jp/12438664/)
+- 地デジTSファイル編集｜eggtoothcrocの日記  
+[http://d.hatena.ne.jp/eggtoothcroc/20100514/p1](http://d.hatena.ne.jp/eggtoothcroc/20100514/p1)
+- パソコン・ハードディスク入門｜高作義明  
+ISBN：4-06-132800-ｘ
